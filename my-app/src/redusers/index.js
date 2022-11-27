ade440eb7445235f9d69a3a9f1f7180c97c759e7
Kseniya <pkorelokq@gmail.com>
2022-11-28 01:35:20 +0300

    change 3

--- my-app/src/redusers/index.js
+++ my-app/src/redusers/index.js
@@ -1,15 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import { combineReducers } from "redux";
 
-import App from "./App";
+import books from "./books";
 
-import createStore from "./store";
-const store = createStore();
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+export default () => combineReducers ({
+  books
+});
