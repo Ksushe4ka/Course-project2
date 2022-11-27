edf55a3dadb122d9b4186f7de9c1fdee2502c86e
Kseniya <pkorelokq@gmail.com>
2022-11-28 01:27:47 +0300

    change 2

--- my-app/src/redusers/index.js
+++ my-app/src/redusers/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+
 import App from "./App";
 
 import createStore from "./store";
 const store = createStore();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
---
+++ my-app/src/store.js
@@ -0,0 +1,9 @@
+import {  legacy_createStore as createStore, applyMiddleware } from 'redux';
+import logger from "redux-logger";
+
+import rootReduser from "./redusers";
+
+export default () => {
+    const store = createStore(rootReduser, applyMiddleware(logger));
+    return store;
+};
