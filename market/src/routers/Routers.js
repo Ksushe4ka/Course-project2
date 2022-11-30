69a975354dad4add8bc009ff9582e55696b1badc
Kseniya <pkorelokq@gmail.com>
2022-11-30 18:03:56 +0300

    change 5

--- market/src/routers/Routers.js
+++ market/src/routers/Routers.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
 import Cart from "../pages/Cart";
@@ -8,15 +8,18 @@ import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 
 const Routers = () => {
-  return <Routes>
-    <Route path='home' element={<Home/>} />    
-    <Route path='shop' element={<Shop/>} />
-    <Route path='shop/:id' element={<BookDetails/>} />
-    <Route path='cart' element={<Cart/>} />
-    <Route path='сheckout' element={<Checkout/>} />
-    <Route path='login' element={<Login/>} />
-    <Route path='signup' element={<Signup/>} />
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/" />} />
+      <Route path="home" element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="shop/:id" element={<BookDetails />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="сheckout" element={<Checkout />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+    </Routes>
+  );
 };
 
 export default Routers;
